8165f0b6d7b840bcc7e9b5604e8e2a3a407d9701
Mark Varley <mark@envision-it.co.uk>
2016-02-09 20:01:23 +0100

    Small amendment for http get call which ensures header key is passed correctly and triggers angularjs to force CORS behaviour

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 var app = angular.module('testApp',[]);
 
 app.controller('mainCtrl', function($scope, $q, $http){
-    
+
     $scope.submitAddress = function(searchAddress){
-        
-        var deferred = $q.defer();
-    
-        $http.get("https://blahblablah/match/v1/address/geocode?query="+searchAddress+"&country=gb&multimatch=all", { "x-api-key": "blahblablah" })
-               .then(function (response) {
-                   deferred.resolve(response);
-               }, function (responseError) {
-                   deferred.reject(responseError);
-               });
-        
-        deferred.promise.then(function(response){
-            $scope.result = response;
-        })
+
+        $http({
+            method: 'GET',
+            url: 'https://blahblablah/match/v1/address/geocode?query=' + searchAddress + '&country=gb&multimatch=all',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Api-Key': 'blahblablah'
+            }
+        }).then(
+            function success(res) {
+                console.log(res);
+            },
+            function error(res) {
+                console.log(res);
+            }
+        );
+
     }
-    
+
 })
